0bde431c63b6874067d5006d1aea686ac5e90dbf
lining01 <lining01@xiaoshouyi.com>
2018-08-23 13:59:03 +0800

    permgen oom by cglib

---
+++ src/main/java/com/hhg/jerry/PermGenByCgLib.java
@@ -0,0 +1,51 @@
+package com.hhg.jerry;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+/**
+ * Created by lining on 2018/8/23.
+ */
+public class PermGenByCgLib {
+    static class Dummy{
+        public void add(){
+            System.out.println("add");
+        }
+
+        public int get(int i){
+            return i;
+        }
+    }
+
+    public static void main(String[] args) {
+        Dummy dummy = getEnhancedObject(Dummy.class);
+        dummy.get(10);
+//        permGenSpaceError();
+        System.out.println("over");
+    }
+
+    public static void permGenSpaceError(){
+        while (true) {
+            getEnhancedObject(Dummy.class);
+        }
+    }
+
+    public static <T> T getEnhancedObject(Class<? extends Object> cls){
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(cls);
+        enhancer.setUseCache(false);
+        enhancer.setCallback(new MethodInterceptor() {
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                System.out.println("before");
+                Object result =  methodProxy.invokeSuper(o, objects);
+                System.out.println("after");
+                return result;
+            }
+        });
+        return (T)enhancer.create();
+    }
+}
